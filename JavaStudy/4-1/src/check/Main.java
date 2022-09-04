e0e88490e659fe444dfa4504bd93e7ba29061d9e
ak750 <110705685+ak750@users.noreply.github.com>
2022-09-04 10:51:00 +0900

    チェックテスト-Java４章
    「Main.java」
    「Pet.java」
    「Constants.java」

---
+++ JavaStudy/4-1/src/check/Main.java
@@ -0,0 +1,23 @@
+package check;
+
+import constants.*;
+
+public class Main {
+	private static final String firstName = "Furudoi";
+	private static final String lastName = "Akira";
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		printName(firstName, lastName);
+		
+		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
+		pet.introduce();
+		RobotPet robotPet = new RobotPet( Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
+		robotPet.introduce();
+	}
+	
+	private static void printName(String firstName, String lastName) {
+		String name = firstName + lastName;
+		System.out.println("printNameメソッド → " + name);
+	}
+}
