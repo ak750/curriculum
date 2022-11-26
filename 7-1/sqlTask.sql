c152a1fa7f135b397b6945d2bb65ea09f70d573e
ak750 <110705685+ak750@users.noreply.github.com>
2022-11-26 18:09:44 +0900

    7章チェックテスト
    SQL課題の再提出

--- 7-1/sqlTask.sql
+++ 7-1/sqlTask.sql
@@ -20,16 +20,29 @@
  
 -- 5. 店舗コード='EA01'の在庫数の平均値より大きい在庫数を持つ店舗コードを抽出しなさい。
 -- [回答]
-      SELECT store_code, AVG(quantity) FROM stock_table GROUP BY store_code HAVING AVG(quantity) > 17.34
+      SELECT store_code FROM stock_table GROUP BY store_code HAVING AVG(quantity) > 
+      (SELECT AVG(quantity) FROM stock_table WHERE store_code = 'EA01')
  
 -- 6. 商品テーブルに「商品コード='M001'、商品名='マフラー'、単価=4500円、更新日付=本日日付」のデータを追加しなさい。※実行後のSELECT結果も貼付すること。
 -- [回答]
+      INSERT INTO goods_table(goods_code, goods_name, price, update_day)
+      VALUES('M001','マフラー','4500','2022-11-25')
+
       SELECT * FROM goods_table
  
 -- 7. 在庫テーブルの商品コード='S987'、かつ、店舗コード='EA01'に対して、「在庫数=10、更新日付=本日日付」で更新しなさい。※実行後のSELECT結果も貼付すること。
 -- [回答]
+      UPDATE stock_table
+      SET
+      quantity = '10',
+      update_day = '2022-11-26'
+      WHERE
+      goods_code = 'S987' AND store_code = 'EA01'
+
       SELECT * FROM stock_table
  
 -- 8. 7で更新した商品を削除しなさい。※実行後のSELECT結果も貼付すること。
 -- [回答]
+      DELETE FROM stock_table WHERE goods_code = 'S987' AND store_code = 'EA01'
+
       SELECT * FROM stock_table
